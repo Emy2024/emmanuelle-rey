df12822e72144f3fc0300ca3bc7f6a0d119268ed
Rey <emmanuelle.rey@outlook.com>
2025-04-15 22:14:38 +0200

    Modification du format de l'image principale

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,8 +22,8 @@ function Home(){
             <div className="mainContent__cover"></div>
               <img className="image" 
                 src={MainPictureDesktop} alt="Image représentant une femme qui tape sur son clavier d'ordinateur" 
-                srcSet={`${MainPictureMobile} 360w, ${MainPictureDesktop} 800w`}
-                sizes="(max-width: 1024px) 360px, 800px"
+                srcSet={`${MainPictureMobile} 360w, ${MainPictureMobile} 450w, ${MainPictureDesktop} 800w`}
+                sizes="(max-width: 1024px) 360px, 450px, 800px"
               />
             </div>
         </div>
