152ec2f9ee7637140827d159bf34b40a226c45f7
Rey <emmanuelle.rey@outlook.com>
2025-04-16 22:34:58 +0200

    Correction srcset

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,9 +21,9 @@ function Home(){
           <div className="mainContent__image">
             <div className="mainContent__cover"></div>
               <img className="image" 
-                src={MainPictureDesktop} alt="Image représentant une femme qui tape sur son clavier d'ordinateur" 
                 srcSet={`${MainPictureMobile} 480w, ${MainPictureDesktop} 1024w`}
-                sizes="(max-width: 1024px) 480px, 1024px"
+                src={MainPictureDesktop}
+                alt="Image représentant une femme qui tape sur son clavier d'ordinateur"
               />
             </div>
         </div>
